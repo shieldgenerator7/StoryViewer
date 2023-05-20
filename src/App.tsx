2eb5989d2b311013cab6e868e0d3a5ae03fbe6a7
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-05-20 01:19:44 -0400

    Add FrontDesk component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 import Message from "./Components/Message";
+import FrontDesk from "./Components/FrontDesk";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Message />
+      <FrontDesk />
     </>
   );
 }
---
+++ src/Components/FrontDesk.tsx
@@ -0,0 +1,28 @@
+"use strict";
+
+function FrontDesk() {
+  let loadText = function (url: string) {
+    if (url) {
+      alert(url);
+    }
+  };
+
+  return (
+    <>
+      Enter URL of story:
+      <input
+        id="txtURL"
+        className="url"
+        placeholder="https://raw.githubusercontent.com/user/repository/file"
+      ></input>
+      <button
+        id="btnLoad"
+        onClick={() => loadText(document.getElementById("txtURL")?.value)}
+      >
+        GO
+      </button>
+    </>
+  );
+}
+
+export default FrontDesk;
