606af0528e3cb0e391e0164bc15432727b1fb9d6
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-05-21 04:18:38 -0400

    Refactor: extract load methods into Load utility

--- src/Components/FrontDesk.tsx
+++ src/Components/FrontDesk.tsx
@@ -1,49 +1,15 @@
 "use strict";
 import { Story } from "../System/Story";
 import { StoryInfo } from "../System/StoryInfo";
-import * as Fetch from "../Utility/Fetch";
+import * as Load from "../Utility/Load";
 
 interface Props {
     label: string;
     setStory: (story: Story) => void;
-    setStoryInfo?: (storyInfo: StoryInfo) => void;
+    setStoryInfo: (storyInfo: StoryInfo) => void;
 }
 
 function FrontDesk({ label, setStory, setStoryInfo }: Props) {
-    let loadText = function (url: string) {
-        if (url) {
-            Fetch.fetchFile(url, (text: string) => {
-                if (url.endsWith(".md")) {
-                    processMD(text);
-                } else if (url.endsWith(".json")) {
-                    processJSON(text, url);
-                }
-            });
-        }
-    };
-
-    let processMD = function (text: string) {
-        setStory(new Story(text));
-    };
-
-    let processJSON = function (text: string, url: string) {
-        if (!setStoryInfo) {
-            return;
-        }
-        let storyInfo = new StoryInfo();
-        let urls = JSON.parse(text);
-        let baseURL = url.substring(0, url.lastIndexOf("/")) + "/";
-        Fetch.fetchFile(baseURL + urls.story, (txt: string) => {
-            storyInfo.story = new Story(txt);
-            setStoryInfo?.(storyInfo);
-        });
-        Fetch.fetchFile(baseURL + urls.characters, (txt: string) => {
-            storyInfo.characters = new Story(txt);
-            setStoryInfo?.(storyInfo);
-        });
-        setStoryInfo?.(storyInfo);
-    };
-
     return (
         <>
             Enter URL of {label}:
@@ -55,12 +21,14 @@ function FrontDesk({ label, setStory, setStoryInfo }: Props) {
             <button
                 id="btnLoad"
                 onClick={() =>
-                    loadText(
+                    Load.loadFile(
                         (
                             document.getElementById(
                                 `txtURL${label}`
                             ) as HTMLInputElement
-                        )?.value
+                        )?.value,
+                        setStory,
+                        setStoryInfo
                     )
                 }
             >
---
+++ src/Utility/Load.ts
@@ -0,0 +1,43 @@
+"use strict";
+import * as Fetch from "../Utility/Fetch";
+import { StoryInfo } from "../System/StoryInfo";
+import { Story } from "../System/Story";
+
+export function loadFile(
+    url: string,
+    setStory: (story: Story) => void,
+    setStoryInfo: (storyInfo: StoryInfo) => void
+) {
+    if (url) {
+        Fetch.fetchFile(url, (text: string) => {
+            if (url.endsWith(".md")) {
+                processMD(text, setStory);
+            } else if (url.endsWith(".json")) {
+                processJSON(text, url, setStoryInfo);
+            }
+        });
+    }
+}
+
+export function processMD(text: string, setStory: (story: Story) => void) {
+    setStory(new Story(text));
+}
+
+export function processJSON(
+    text: string,
+    url: string,
+    setStoryInfo: (storyInfo: StoryInfo) => void
+) {
+    let storyInfo = new StoryInfo();
+    let urls = JSON.parse(text);
+    let baseURL = url.substring(0, url.lastIndexOf("/")) + "/";
+    Fetch.fetchFile(baseURL + urls.story, (txt: string) => {
+        storyInfo.story = new Story(txt);
+        setStoryInfo?.(storyInfo);
+    });
+    Fetch.fetchFile(baseURL + urls.characters, (txt: string) => {
+        storyInfo.characters = new Story(txt);
+        setStoryInfo?.(storyInfo);
+    });
+    setStoryInfo?.(storyInfo);
+}
