0e38b9fc373af382cee24d8fc00b49cae9279a47
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-08-19 02:08:43 -0400

    Add Character.id

--- src/System/Character.js
+++ src/System/Character.js
@@ -4,6 +4,7 @@ export class Character {
     constructor() {
         this.name = "unnamed";
         this.nicknames = [];
+        this.id = "AAA";//the short id used to identify characters when it's not obvious, format: @AAA
         this.portrait = -1; //image
         this.portraitURL = " ";
         this.portraitCredit = " ";
