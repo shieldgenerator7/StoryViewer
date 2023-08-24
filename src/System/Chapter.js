dc2e39d4bfe299080726f69b9ceacb229f869d7b
shieldgenerator7 <shieldgenerator7@yahoo.com>
2023-08-24 04:47:07 -0400

    Chapters number for Title, Prologue, Epilogue

--- src/System/Chapter.js
+++ src/System/Chapter.js
@@ -2,21 +2,44 @@
 import { Paragraph } from "../System/Paragraph";
 
 export class Chapter {
-    constructor(text, number) {
+    constructor(text, index) {
         this.text = text;
-        this.number = number;
+        this.number = undefined;
+        this.index = index;
         this._lines = text.split('\n').map(line => line.trim()).filter(line => line);
         this.title = this._lines[0];
         this.title1 = this.title.split(" ")[0].trim();
         this.paragraphs = this._lines.map(line => new Paragraph(line));
+        //
+        this.analyzeTitle();
     }
 
-    analyze(characterList) {
+    analyzeTitle() {
         //analyze chapter number
+        //title has number
         let number = this.title.match(/[0-9]+/)?.[0];
         if (number != undefined) {
             this.number = number;
+            return;
         }
+        //title is prologue
+        if (this.title.toLowerCase().includes("prologue")) {
+            this.number = "P";
+            return;
+        }
+        //title is epilogue
+        if (this.title.toLowerCase().includes("epilogue")) {
+            this.number = "E";
+            return;
+        }
+        //Default: Title is Title
+        if (this.index == 0) {
+            this.number = "T";
+            return;
+        }
+    }
+
+    analyze(characterList) {
         //analyze characters
         let lastParagraph = undefined;
         this.paragraphs.forEach(p => {
@@ -27,10 +50,24 @@ export class Chapter {
     }
 
     getLabelShort() {
+        //Title
+        if (this.number == "T") {
+            return "Title";
+        }
+        //Prologue
+        else if (this.number == "P") {
+            return "Prologue";
+        }
+        //Epilogue
+        else if (this.number == "E") {
+            return "Epilogue";
+        }
+        //Chapter #
         let isNumber = /|[0-9]+|/.test(`|${this.number}|`);
         if (isNumber) {
-            return (this.number == 0) ? "Title" : `Ch${this.number}`;
+            return `Ch${this.number}`;
         }
-        return this.number;
+        //Index
+        return `Ch${this.index}`;
     }
 }
